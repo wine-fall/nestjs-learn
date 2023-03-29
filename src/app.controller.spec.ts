5d52a5b6bc2e5a530319bcfb36db847fbc946950
guozhiyi01 <guozhiyi01@baidu.com>
2023-03-29 17:14:09 +0800

    feat: init request hello world!

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,3 +1,22 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+
 describe('AppController', () => {
-  return;
+  let authController: AuthController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AuthController],
+      providers: [AuthService],
+    }).compile();
+
+    authController = app.get<AuthController>(AuthController);
+  });
+
+  describe('root', () => {
+    it('should return "signin"', () => {
+      expect(authController.signin()).toBe('signin');
+    });
+  });
 });
