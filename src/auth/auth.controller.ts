5937ad33e85a158edc0a5a6ee0e4dc41b37f99d9
guozhiyi01 <guozhiyi01@baidu.com>
2023-03-29 16:58:45 +0800

    feat: controller init

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private autchService: AuthService) {}
+
+  //   @Post('hello/world')
+  //   helloWorld() {
+  //     return {
+  //       msg: 'hello world!',
+  //     };
+  //   }
+
+  @Post('signup')
+  signup() {
+    return 'signup';
+  }
+
+  @Post('signin')
+  signin() {
+    return 'signin';
+  }
+}
